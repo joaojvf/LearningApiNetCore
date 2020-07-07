d873b7618a82640df6d09e6ec7eb6c9faf8adbc8
joaojvf <joao.victorf02@hotmail.com>
2020-07-07 14:35:53 -0300

    Projeto de estudo sobre testes

---
+++ LearningTests/Calculations/Calculations.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace Calculations.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
