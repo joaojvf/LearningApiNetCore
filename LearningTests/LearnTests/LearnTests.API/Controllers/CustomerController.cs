7db432f062959682d540491c9abf286fcd5df048
Caio Humberto Francisco <caio.francisco@ltm.digital>
2020-07-22 18:20:51 -0300

    refactory: downgrade do aspnet.core e ajustes nas dependencias

--- LearningTests/LearnTests/LearnTests.API/Controllers/CustomerController.cs
+++ LearningTests/LearnTests/LearnTests.API/Controllers/CustomerController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using LearnTests.Domain.Entities;
+﻿using LearnTests.Domain.Entities;
 using LearnTests.Domain.Interfaces.Repositories;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LearnTests.API.Controllers
--- LearningTests/LearnTests/LearnTests.API/Startup.cs
+++ LearningTests/LearnTests/LearnTests.API/Startup.cs
@@ -2,52 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using LearnTests.Domain.Interfaces.Repositories;
-using LearnTests.Infra.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace LearnTests.API
+namespace WebApplication1
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-            services.ResolveIoCDependecy();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            app.Run(async (context) =>
             {
-                endpoints.MapControllers();
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
